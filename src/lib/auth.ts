a748885c3ee55d920de8fcb0c7c1643f9c37ce4c
Tasin <tasinahmed4482@gmail.com>
2026-01-31 12:21:39 +0600

    restore cookie storage

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -34,11 +34,6 @@ export const auth = betterAuth({
     advanced: {
         useSecureCookies: true,
     },
-    secondaryStorage: {
-        get: async (key) => null,
-        set: async (key, value) => { },
-        delete: async (key) => { }
-    },
     user: {
         additionalFields: {
             role: { type: "string", defaultValue: "CUSTOMER" },
