fb0a1090126094650e90917f967bf580fd290f23
Tasin <tasinahmed4482@gmail.com>
2026-02-03 16:07:06 +0600

    Fix cookie domain issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -46,7 +46,6 @@ export const auth = betterAuth({
             sameSite: "none",
             secure: true,
             httpOnly: true,
-            domain: ".vercel.app",
         },
         disableCSRFCheck: true,
     },
